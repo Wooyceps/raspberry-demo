d135682561a320475a5b7408fe17314cf6183063
Michał Wójcik <mwooycik@gmail.com>
2024-05-01 16:30:02 +0200

    basic, remotely executed led blink

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+import RPi.GPIO as GPIO
+import time
 
+# Set up GPIO using BCM numbering
+GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)
+GPIO.setup(11, GPIO.OUT)
+
+# Simple blink script
+for i in range(5):
+    GPIO.output(11, GPIO.HIGH)
+    time.sleep(1)
+    GPIO.output(11, GPIO.LOW)
+    time.sleep(1)
